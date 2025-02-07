6bc141917e7c04fc329d99eb74fb7dc80edcf246
kbatrakov <kirillbatrakov0819@gmail.com>
2025-02-07 22:28:57 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -4,36 +4,24 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.wife = None
-        self.husband = None
-        if self.name not in Person.people:
-            Person.people.update({
-                self.name: self
-            })
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
     instances = []
+
     for person in people:
         instance = Person(person["name"], person["age"])
-        if "wife" in person:
-            instance.wife = person["wife"]
-        if "husband" in person:
-            instance.husband = person["husband"]
+        if person.get("wife") is not None:
+            instance.wife = Person.people.get(person["wife"])
+        if person.get("husband") is not None:
+            instance.husband = Person.people.get(person["husband"])
         instances.append(instance)
 
     for instance in instances:
-        if (instance.wife and instance.wife is not None or instance.husband
-                and instance.husband is not None):
-            for partner in instances:
-                if instance.wife == partner.name:
-                    instance.wife = partner
-                    break
-                if instance.husband == partner.name:
-                    instance.husband = partner
-                    break
-        else:
-            del instance.wife
-            del instance.husband
+        if hasattr(instance, "wife") and instance.wife:
+            instance.wife.husband = instance
+        if hasattr(instance, "husband") and instance.husband:
+            instance.husband.wife = instance
 
     return instances
